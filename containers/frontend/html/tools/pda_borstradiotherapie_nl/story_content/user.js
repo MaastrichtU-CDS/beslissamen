ac309a171290cea4c3d87ac1431cc7c082fbdf1d
Johan van Soest <johan.vansoest@maastro.nl>
2018-10-17 11:28:46 +0200

    Updated breast RT PDA according to delivered content

--- containers/frontend/html/tools/pda_borstradiotherapie_nl/story_content/user.js
+++ containers/frontend/html/tools/pda_borstradiotherapie_nl/story_content/user.js
@@ -1,1 +1,55 @@
-// User defined code goes here
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5eUozhsLWIe":
+        Script1();
+        break;
+      case "6iPddAQ5y6x":
+        Script2();
+        break;
+      case "638JCFfCN5L":
+        Script3();
+        break;
+      case "5stblwv6AR3":
+        Script4();
+        break;
+      case "5cRVemo1hk2":
+        Script5();
+        break;
+      case "69bvyKY05z2":
+        Script6();
+        break;
+  }
+}
+
+function Script1()
+{
+  window.frames[0].frameElement.contentWindow.createPDF();
+}
+
+function Script2()
+{
+  window.frames[0].frameElement.contentWindow.createPDF();
+}
+
+function Script3()
+{
+  window.frames[0].frameElement.contentWindow.createPDF();
+}
+
+function Script4()
+{
+  window.frames[0].frameElement.contentWindow.createPDF();
+}
+
+function Script5()
+{
+  window.frames[0].frameElement.contentWindow.createPDF();
+}
+
+function Script6()
+{
+  window.frames[0].frameElement.contentWindow.createPDF();
+}
+
