8fc90de46db944d94f7f7bebee1e66b032937b09
Johan van Soest <info@johanvansoest.nl>
2018-12-23 18:35:17 +0000

    Update setup_base.sh

--- run_scripts/acc/setup_base.sh
+++ run_scripts/acc/setup_base.sh
@@ -1,9 +1,11 @@
+postgresPass=$(</data/beslissamen/acc/containers/mysql/password.txt)
+
 docker network create beslissamen_acc
 
 docker run -d \
     --name beslis_acc_db \
     --network=beslissamen_acc \
     -v /data/beslissamen/acc/database/:/var/lib/postgresql/data/ \
-    -e POSTGRES_USER=beslissamen \
+    -e POSTGRES_USER=$postgresPass \
     -e POSTGRES_PASSWORD=AblexaVerEsT \
     postgres:10.4
