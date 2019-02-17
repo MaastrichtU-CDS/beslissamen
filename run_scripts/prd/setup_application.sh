e447bc4deddf6f03dc7cc9126463fe2a218e82c5
Johan van Soest <johan.vansoest@maastro.nl>
2019-02-17 16:54:57 +0100

    Fixed application and base setup for production

--- run_scripts/prd/setup_application.sh
+++ run_scripts/prd/setup_application.sh
@@ -9,13 +9,13 @@ docker rm beslis_prd_frontend
 docker run -d \
     --name beslis_prd_backend \
     --network=beslissamen_prd \
-    -v $(pwd)/containers/backend/application.yml:/app/application.yml \
+    -v /data/beslissamen/prd/containers/backend/application.yml:/app/application.yml \
     --link beslis_prd_db:dbhost \
-    -p 10081:8081 \
+    -p 30081:8081 \
     registry.gitlab.com/medicaldataworks/beslissamen/backend:$1
 
 docker run -d \
     --name beslis_prd_frontend \
     --network=beslissamen_prd \
-    -p 10080:80 \
+    -p 30080:80 \
     registry.gitlab.com/medicaldataworks/beslissamen/frontend:$1
--- run_scripts/prd/setup_base.sh
+++ run_scripts/prd/setup_base.sh
@@ -1,9 +1,20 @@
+#!/bin/bash
+
+dbRunning=$(docker inspect -f '{{.State.Running}}' beslis_prd_db)
+if [[ $dbRunning == "true" ]]
+then
+    echo "Skipping base setup"
+    exit 0
+fi
+
+postgresPass=$(</data/beslissamen/prd/containers/mysql/password.txt)
+
 docker network create beslissamen_prd
 
 docker run -d \
     --name beslis_prd_db \
-    --network=beslissamen_acc \
-    -v $(pwd)/database_prd/:/var/lib/postgresql/data/ \
+    --network=beslissamen_prd \
+    -v /data/beslissamen/prd/database/:/var/lib/postgresql/data/ \
     -e POSTGRES_USER=beslissamen \
-    -e POSTGRES_PASSWORD=AblexaVerEsT \
+    -e POSTGRES_PASSWORD=$postgresPass \
     postgres:10.4
